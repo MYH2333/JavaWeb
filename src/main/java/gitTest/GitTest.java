484b5ded94789765656b3b5c0fbdbe9ef613bb3a
MYH2333 <56840111+MYH2333@users.noreply.github.com>
2021-04-29 15:36:02 +0800

    test pull

--- src/main/java/gitTest/GitTest.java
+++ src/main/java/gitTest/GitTest.java
@@ -3,12 +3,7 @@ package gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("second commit");
-        System.out.println("hot-fix third commit");
-        System.out.println("hot-fix test");
-
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
+        
 
     }
 }
