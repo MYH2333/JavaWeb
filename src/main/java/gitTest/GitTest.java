4e3bfb9a49b56eb060dc6851ff18aba48b5cc29f
myh <1337704156@qq.com>
2021-04-29 15:28:54 +0800

    hot-fix first commit2

--- src/main/java/gitTest/GitTest.java
+++ src/main/java/gitTest/GitTest.java
@@ -4,8 +4,11 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("second commit");
         System.out.println("hot-fix third commit");
-
+        System.out.println("hot-fix test");
 
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
+
     }
 }
